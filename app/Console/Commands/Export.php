8fb56cdb090d71d37377571baabf7b1e9087efd4
Willy Nugraha <willymiiee@gmail.com>
2019-05-12 00:51:22 +0800

    Add Export command to artisan console

---
+++ app/Console/Commands/Export.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Export extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'export';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Download & export the data from S3';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+    }
+}
